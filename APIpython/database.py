7f13860e227caeae62c15e53a50f7f1e4370c57b
Felipe-Ortega <felipe.osouza@sptech.school>
2025-02-27 20:10:29 -0300

    Usuário do banco alterado

--- APIpython/database.py
+++ APIpython/database.py
@@ -4,8 +4,8 @@ from mysql.connector import (connection)
 def gerarMyDb():
     return connection.MySQLConnection(
             host='localhost',          
-            user='',        
-            password='',      
+            user='machine',        
+            password='machineMYDB@01',      
             database='TradeFlux',
             auth_plugin='mysql_native_password',
             ssl_disabled= True
