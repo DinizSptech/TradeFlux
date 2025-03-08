aa969917da832dd2f1a303fee15f80c8dc8c0df6
Felipe-Ortega <felipe.osouza@sptech.school>
2025-03-08 20:53:55 -0300

    Separação de responsabilidades de usuários no banco feita

--- database/user.sql
+++ database/user.sql
@@ -1,7 +1,7 @@
 CREATE USER 'user_insert'@'%' IDENTIFIED BY 'machineMYDB@01';
-GRANT INSERT ON *.* TO 'machine'@'%';
+GRANT INSERT ON *.* TO 'user_insert'@'%';
 FLUSH PRIVILEGES;
 
 CREATE USER 'user_select'@'%' IDENTIFIED BY 'machineMYDB@01';
-GRANT SELECT ON *.* TO 'machine'@'%';
+GRANT SELECT ON *.* TO 'user_select'@'%';
 FLUSH PRIVILEGES;
