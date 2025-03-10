c2a7fe12143dd9caf8cd3989ffd1baf67efc53da
Felipe-Ortega <felipe.osouza@sptech.school>
2025-03-10 10:31:02 -0300

    API finalizada

--- APIpython/extract.py
+++ APIpython/extract.py
@@ -4,26 +4,33 @@ import time
 import math
 
 
-idMaquina = 3
+idMaquina = 1
 
 def coletarCPU():
     cpu = psutil.cpu_percent()
     return cpu
 
 def coletarRam():
-    ramUsada = round((psutil.virtual_memory().used) / (1024 ** 3), 2)
+    ramUsada = round(psutil.virtual_memory().percent, 2)
     return ramUsada
 
 def coletarDisk():
-    usedDisk = math.ceil((psutil.disk_usage("/").used) / (1024 ** 3))
+    usedDisk = round(psutil.disk_usage("/").percent, 2)
     return usedDisk
+
+def coletarPorCpuLogico():
+    listaCPUs = psutil.cpu_percent(interval=1, percpu=True)
+    for i in range(len(listaCPUs)):
+            usoCPUs += f"Uso do {i + 1}° núcleo: {listaCPUs[i]}%\n"
+            if listaCPUs[i] > 70:
+                alertaCPUs += f"O {i}° está com o uso acima de 70%!!!!!\n"
 
 def coletaLocal():
     while True:
         ram = coletarRam()
         disk = coletarDisk()
         cpu = coletarCPU()
-        if cpu or disk or ram > 70:
+        if cpu > 70 or disk > 70 or ram > 70:
             insert.inserirAlert(idMaquina, cpu, ram, disk)
         insert.inserirData(idMaquina, cpu, ram, disk)
         time.sleep(2)
--- APIpython/insert.py
+++ APIpython/insert.py
@@ -4,13 +4,13 @@ mydb = database.gerarMyDbInsert()
 cursor = mydb.cursor()
 
 def inserirData(idMaquina, cpu, ram, disk):
-    query = f"INSERT INTO Data (idMachine, cpuUsage, ramUsage, diskUsage) VALUES('{idMaquina}', '{cpu}', '{ram}', '{disk}');"
+    query = f"INSERT INTO Data (idMachine, cpuUsage, ramUsage, diskUsage) VALUES({idMaquina}, '{cpu}', '{ram}', '{disk}');"
     cursor.execute(query)
     mydb.commit()
 
 
 def inserirAlert(idMaquina, cpu, ram, disk):
-    query = f"INSERT INTO Alert (fkMachine, cpuUsage, ramUsage, diskUsage) VALUES('{idMaquina}', '{cpu}', '{ram}', '{disk}');"
+    query = f"INSERT INTO Alert (fkMachine, cpuUsage, ramUsage, diskUsage) VALUES({idMaquina}, '{cpu}', '{ram}', '{disk}');"
     cursor.execute(query)
     mydb.commit()
 
