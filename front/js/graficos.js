844522a64186a1c157104939c80a49032e363f73
Gustavo Vieira Felicio Amorim <160238593+GustavoVFA-SPTECH@users.noreply.github.com>
2025-03-11 21:23:34 -0300

    adição node

---
+++ front/js/graficos.js
@@ -0,0 +1,8 @@
+const ctx = document.getElementById("mychart")
+const ctx2 = document.getElementById("mychart2")
+const ctx3 = document.getElementById("mychart3")
+
+
+const chart = new Chart(ctx, {
+    
+})
