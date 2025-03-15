d4dd50dbf55de35bfbfa4df532af2e2b951a769a
Felipe-Ortega <felipe.osouza@sptech.school>
2025-03-15 16:32:45 -0300

    Deixar input de numero disabled quando usuário informar que não há número

--- front/js/cadastroEmpresa.js
+++ front/js/cadastroEmpresa.js
@@ -1,34 +1,38 @@
-function coletarInputs(){
+function coletarInputs() {
     var razaoSocial = ipt_rzsocial.value;
     var cnpj = ipt_cnpj.value;
     var telefone = ipt_numero.value;
     var cep = ipt_cep.value;
-    var erro = false;
+
     if (!validarRazaoSocial(razaoSocial)) {
         div_erro_rzsocial.innerText = "Razão Social inválida.";
-        erro = true;
+        return; 
     } else {
         div_erro_rzsocial.innerText = "";
     }
+
     if (!validarCnpj(cnpj)) {
         div_erro_cnpj.innerText = "CNPJ inválido. Formato esperado: 00.000.000/0000-00";
-        erro = true;
+        return; 
     } else {
         div_erro_cnpj.innerText = "";
     }
+
     if (!validarTelefone(telefone)) {
         div_erro_num.innerText = "Número de telefone inválido. Formato esperado: (XX) XXXXX-XXXX";
-        erro = true;
+        return;
     } else {
         div_erro_num.innerText = "";
     }
+
     if (!validarCep(cep)) {
-        alert("CEP inválido. Formato esperado: 00000-000");
-        erro = true;
+        div_erro_cep.innerText = "CEP inválido. Formato esperado: 00000-000";
+        return;
+    } else {
+        div_erro_cep.innerText = "";
     }
-    if (validarCep && validarCnpj && validarRazaoSocial && validarTelefone) {
-        alert("Cadatro realizado com sucesso!")
-    }
+
+    alert("Cadastro realizado com sucesso!");
 }
 
 function validarTelefone(telefone) {
@@ -50,3 +54,11 @@ function validarCnpj(cnpj) {
     var regexCnpj = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
     return regexCnpj.test(cnpj);
 }
+
+function desabilitaInputNumero() {
+    var checkbox = document.getElementById("ipt_n_possui_num");
+    var inputNumero = document.getElementById("ipt_num");
+    inputNumero.disabled = checkbox.checked;
+}
+var checkbox = document.getElementById("ipt_n_possui_num");
+checkbox.addEventListener("change", desabilitaInputNumero);
