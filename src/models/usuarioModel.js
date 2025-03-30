e240432623957b1c346cfd651e05ae19e1f2a7f8
victorsantos41 <victor.santos047@sptechschool>
2025-03-30 18:34:05 -0300

    Configurando backend do cadastroUsuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,6 +1,5 @@
 const database = require("../database/config");
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrarUsuario(nome, senha, email, cargo, ativo, fk_cliente) {
   console.log(
     "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
