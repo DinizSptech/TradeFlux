c572300e6c58dfec7f0fa344dfd4a005e03c6336
amandampedroza <amanda.pedroza@sptech.school>
2025-04-12 15:10:37 -0300

    cadastro do usuario com a modelagem atual

--- database/scriptComDados.sql
+++ database/scriptComDados.sql
@@ -1,5 +1,7 @@
 -- NOVO SCRIPT SQL 
-	
+
+show tables;
+
 CREATE DATABASE IF NOT EXISTS tradeflux;
 use tradeflux;
 
@@ -38,7 +40,9 @@ CREATE TABLE IF NOT EXISTS Usuario_Cliente (
     senha VARCHAR(100),
     cargo VARCHAR(45),
     ativo TINYINT(1),
+    fk_cliente INT,
     fkData_Center INT,
+    CONSTRAINT fk_usuario_cliente FOREIGN KEY (fk_cliente) REFERENCES Empresa_Cliente(idCliente),
     CONSTRAINT fk_user_datacenter FOREIGN KEY (fkData_Center) REFERENCES Data_Center(idData_Center)
 );
 
@@ -121,9 +125,9 @@ INSERT INTO Data_Center VALUES
 (default, "B3 Data Center Rio", 1, 2);
 
 INSERT INTO  usuario_cliente VALUES
-(default, "Mateus Diniz Leite", "mateusdiniz@gmail.com","Tradeflux123@", "administrador", 1,1),
-(default, "Ana Paula Ferreira", "ana.paula@xp.com", "senhaForte123!", "analista", 1,2),
-(default, "Carlos Andrade", "c.andrade@petrobras.com", "SenhaSegura456!", "cientista", 1,2
+(default, "Mateus Diniz Leite", "mateusdiniz@gmail.com","Tradeflux123@", "administrador", 1, 1,1),
+(default, "Ana Paula Ferreira", "ana.paula@xp.com", "senhaForte123!", "analista", 1, 1,2),
+(default, "Carlos Andrade", "c.andrade@petrobras.com", "SenhaSegura456!", "cientista", 1,1,2
 );
 
 
@@ -133,12 +137,12 @@ INSERT INTO servidor_cliente VALUES
 (default, "B4C9D1B3-3F2A-48B5-9F72-TESTE2", 64.0, 2000.0, "Intel Xeon Gold", 1
 );
 
-INSERT INTO Componente VALUES
+INSERT INTO Componentes VALUES
 (default, "CPU", "%"),
 (default, "RAM", "%"),
 (default, "Disco", "%");
 
-INSERT INTO Configuracao_Servidor VALUES
+INSERT INTO Parametros_Servidor VALUES
 (default, 85.0, 1, 1),
 (default, 90.0, 1, 2),
 (default, 80.0, 1, 3),
@@ -147,7 +151,12 @@ INSERT INTO Configuracao_Servidor VALUES
 (default, 80.0, 2, 3);
 
 INSERT INTO Captura VALUES
-(default, 72.5, default, 1);
+(default, 1, 72.5),
+(default, 2, 65.3),
+(default, 3, 79.1),
+(default, 4, 60.2),
+(default, 5, 70.4),
+(default, 6, 81.9);
 
 SELECT * FROM captura as c JOIN Parametros_servidor as p ON c.fkParametro = p.idParametro_Servidor;
 
@@ -165,8 +174,6 @@ JOIN servidor_cliente as sc ON dc.idData_Center = sc.fk_data_center;
 SELECT * FROM empresa_cliente;
 
 SELECT * FROM data_center;
-
-select * from Usuario_Cliente;
 
 
 
