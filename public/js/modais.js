8a803dba30c503e2afc2ad1d853e0e7eabf4fd66
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2025-04-14 00:00:43 -0300

    Adicionando modal de notificações na dash de gerenciamento de usuarios

--- public/js/modais.js
+++ public/js/modais.js
@@ -1,4 +1,5 @@
 var status_perfil = "fechado"
+var status_alerta = "fechado"
 function abrirModal(tipo){
     if(tipo == "cadastro"){
         bg_formulario.style.display = "flex";
@@ -7,9 +8,23 @@ function abrirModal(tipo){
     } else if(tipo == "perfil" && status_perfil == "fechado"){
         status_perfil = "aberto"
         perfil_opcoes.style.display = "flex";
+        status_alerta = "fechado"
+        tabela_notificacoes.style.display = "none";
     } else if(tipo == "perfil" && status_perfil ==  "aberto"){
         status_perfil = "fechado"
         perfil_opcoes.style.display = "none";
+    } else if(tipo == "perfil_completo"){
+        status_perfil = "fechado"
+        perfil_opcoes.style.display = "none";
+        bg_modal_perfil_completo.style.display = "flex"
+    } else if(tipo == "notificacoes" && status_alerta == "fechado"){
+        status_alerta = "aberto"
+        tabela_notificacoes.style.display = "flex";
+        status_perfil = "fechado"
+        perfil_opcoes.style.display = "none";
+    } else if(tipo == "notificacoes" && status_alerta ==  "aberto"){
+        status_alerta = "fechado"
+        tabela_notificacoes.style.display = "none";
     } else if(tipo == "perfil_completo"){
         status_perfil = "fechado"
         perfil_opcoes.style.display = "none";
