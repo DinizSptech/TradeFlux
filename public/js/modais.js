b0ca3c4eee9ab5ecc7daa303681a5ac9c2496479
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2025-04-19 00:33:06 -0300

    Ajustes na renderização do modal de perfil em todas as páginas

--- public/js/modais.js
+++ public/js/modais.js
@@ -41,3 +41,49 @@ function abrirModal(tipo){
         bg_modal_perfil_completo.style.display = "none";
     }
   }
+
+  function renderizarModalUsuario(){
+    main_page.innerHTML += `<div class="container_perfil" id="modal_perfil">
+      <i class="fa-solid fa-bell" onclick="abrirModal('notificacoes')"></i>
+      <span class="barraVertical"></span>
+      <div class="button_perfil" onclick="abrirModal('perfil')">
+        <i class="fa-solid fa-circle-user"></i>
+        <i class="fa-solid fa-chevron-down"></i>
+      </div>
+    </div>
+    <div class="container_tabela_notificacoes" id="tabela_notificacoes">
+      <div class="wrapper">
+
+        <span>Alerta 1</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+        <span>Alerta 2</span>
+      </div>
+    </div>
+    <div class="container_perfil_opcoes" id="perfil_opcoes">
+      <span onclick="abrirModal('perfil_completo')">Perfil</span>
+      <span>Sair</span>
+    </div>
+    <div class="bg_modal_perfil_completo" id="bg_modal_perfil_completo">
+      <div class="container_perfil_completo" id="div_perfil_completo">
+        <div class="perfil_conteudo">
+          <i class="fa-solid fa-circle-user" id="circulo_usuario"></i>
+          <div class="div_informacoes" id="seta_usuario">
+            <span class="spn_perfil spn_perfil_nome">Tobias Maria Roberto</span>
+            <span class="spn_perfil">Administrador</span>
+            <span class="spn_perfil">B3</span>
+          </div>
+        </div>
+        <span class="btn_fechar_perfil" onclick="fecharModal('perfil_completo')">Fechar</span>
+      </div>
+    </div>`
+  }
