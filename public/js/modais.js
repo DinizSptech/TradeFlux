82bfce5c7de1064f7b8e770cb3e6e67eebbee696
victorsantos41 <victor.santos047@sptech.school>
2025-04-27 13:41:56 -0300

    Editando funcao modal

--- public/js/modais.js
+++ public/js/modais.js
@@ -1,52 +1,53 @@
-var status_perfil = "fechado"
-var status_alerta = "fechado"
-function abrirModal(tipo){
-    if(tipo == "cadastro"){
-        bg_formulario.style.display = "flex";
-    } else if(tipo == "edicao"){
-        bg_formulario_edicao.style.display = "flex";
-    } else if(tipo == "perfil" && status_perfil == "fechado"){
-        status_perfil = "aberto"
-        perfil_opcoes.style.display = "flex";
-        status_alerta = "fechado"
-        tabela_notificacoes.style.display = "none";
-    } else if(tipo == "perfil" && status_perfil ==  "aberto"){
-        status_perfil = "fechado"
-        perfil_opcoes.style.display = "none";
-    } else if(tipo == "perfil_completo"){
-        status_perfil = "fechado"
-        perfil_opcoes.style.display = "none";
-        bg_modal_perfil_completo.style.display = "flex"
-    } else if(tipo == "notificacoes" && status_alerta == "fechado"){
-        status_alerta = "aberto"
-        tabela_notificacoes.style.display = "flex";
-        status_perfil = "fechado"
-        perfil_opcoes.style.display = "none";
-    } else if(tipo == "notificacoes" && status_alerta ==  "aberto"){
-        status_alerta = "fechado"
-        tabela_notificacoes.style.display = "none";
-    } else if(tipo == "perfil_completo"){
-        status_perfil = "fechado"
-        perfil_opcoes.style.display = "none";
-        bg_modal_perfil_completo.style.display = "flex"
-    }
-
+var status_perfil = "fechado";
+var status_alerta = "fechado";
+function abrirModal(tipo) {
+  if (tipo == "cadastro") {
+    bg_formulario.style.display = "flex";
+  } else if (tipo == "edicao") {
+    bg_formulario_edicao.style.display = "flex";
+  } else if (tipo == "perfil" && status_perfil == "fechado") {
+    status_perfil = "aberto";
+    perfil_opcoes.style.display = "flex";
+    status_alerta = "fechado";
+    tabela_notificacoes.style.display = "none";
+  } else if (tipo == "perfil" && status_perfil == "aberto") {
+    status_perfil = "fechado";
+    perfil_opcoes.style.display = "none";
+  } else if (tipo == "perfil_completo") {
+    status_perfil = "fechado";
+    perfil_opcoes.style.display = "none";
+    bg_modal_perfil_completo.style.display = "flex";
+  } else if (tipo == "notificacoes" && status_alerta == "fechado") {
+    status_alerta = "aberto";
+    tabela_notificacoes.style.display = "flex";
+    status_perfil = "fechado";
+    perfil_opcoes.style.display = "none";
+  } else if (tipo == "notificacoes" && status_alerta == "aberto") {
+    status_alerta = "fechado";
+    tabela_notificacoes.style.display = "none";
+  } else if (tipo == "perfil_completo") {
+    status_perfil = "fechado";
+    perfil_opcoes.style.display = "none";
+    bg_modal_perfil_completo.style.display = "flex";
   }
-  function fecharModal(tipo){
-    if (tipo == "cadastro") {
-        bg_formulario.style.display = "none";
-    } else if(tipo == "edicao"){
-        bg_formulario_edicao.style.display = "none";
-    } else if(tipo == "perfil_completo"){
-        bg_modal_perfil_completo.style.display = "none";
-    }
+}
+function fecharModal(tipo) {
+  if (tipo == "cadastro") {
+    bg_formulario.style.display = "none";
+  } else if (tipo == "edicao") {
+    bg_formulario_edicao.style.display = "none";
+  } else if (tipo == "perfil_completo") {
+    bg_modal_perfil_completo.style.display = "none";
   }
-  function renderizarModalUsuario(){
-    sistema_modais.innerHTML += `<div class="container_perfil" id="modal_perfil">
+}
+function renderizarModalUsuario() {
+  sistema_modais.innerHTML += `<div class="container_perfil" id="modal_perfil">
       <i class="fa-solid fa-bell" onclick="abrirModal('notificacoes')"></i>
       <span class="barraVertical"></span>
       <div class="button_perfil" onclick="abrirModal('perfil')">
-        <i class="fa-solid fa-circle-user"></i>
+      <img id="rogerioCientista" style="display: none; border-radius: 50%;" src="../assets/icons_protopersonas/rogerioCientista.png" alt="" height="80"/>
+      <img id="jenniferADM"      style="display: none; border-radius: 50%;" src="../assets/icons_protopersonas/jenniferADM.png" alt="" height="80"/>
+      <img id="juliaAnalista"    style="display: none; border-radius: 50%;" src="../assets/icons_protopersonas/juliaAnalista.png" alt="" height="80"/>
         <i class="fa-solid fa-chevron-down"></i>
       </div>
     </div>
@@ -84,10 +85,10 @@ function abrirModal(tipo){
         </div>
         <span class="btn_fechar_perfil" onclick="fecharModal('perfil_completo')">Fechar</span>
       </div>
-    </div>`
-    nome_usuario = document.getElementById("nome_usuario")
-    cargo_usuario = document.getElementById("cargo_usuario")
+    </div>`;
+  nome_usuario = document.getElementById("nome_usuario");
+  cargo_usuario = document.getElementById("cargo_usuario");
 
-    nome_usuario.innerHTML = sessionStorage.NOME_USUARIO;
-    cargo_usuario.innerHTML = sessionStorage.Cargo;
- }
+  nome_usuario.innerHTML = sessionStorage.NOME_USUARIO;
+  cargo_usuario.innerHTML = sessionStorage.Cargo;
+}
