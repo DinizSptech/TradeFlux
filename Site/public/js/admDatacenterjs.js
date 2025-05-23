0d1ea85deb333f5a0b9317c10ab7ebba8d0573e9
GuilhermeAugustoFigueiredo <guilherme.figueiredo@sptech.school>
2025-05-23 00:54:33 -0300

    dashboard visão equipe melhorada com UX

--- Site/public/js/admDatacenterjs.js
+++ Site/public/js/admDatacenterjs.js
@@ -1,46 +1,91 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var options = {
-    chart: {
-      type: 'bar',
-      height: 200,
-      width: 650   
-    },
-      responsive: [{
-    breakpoint: 768,
-    options: {
-      chart: {
-        height: 400
-      }
-    }
-  }], 
-    series: [
-      {
-        name: 'Alertas Atrasados',
-        data: [8, 8, 8]
-      },
-      {
-        name: 'Alertas Totais',
-        data: [6, 4, 2]
-      }
-    ],
-    xaxis: {
-      categories: ['Data Center 1', 'Data Center 3', 'Data Center 2']
-    },
-    plotOptions: {  
-      bar: {
-        horizontal: false,
-        columnWidth: '55%',
-        endingShape: 'rounded'
-      }
-    },
-    dataLabels: {
-      enabled: true
-    },
-    legend: {
-      position: 'top'
-    }
-  };
+    var options = {
+        chart: {
+            type: 'bar',
+            height: '100%',
+            width: '100%',
+            toolbar: {
+                show: false
+            },
+            foreColor: '#ffff',
+            fontSize: '30px',
+        },
+        responsive: [{
+            breakpoint: 768,
+            options: {
+                chart: {
+                    height: '100%'
+                },
+                legend: {
+                    position: 'bottom',
+                    fontSize: '8px'
+                },
+                dataLabels: {
+                    style: {
+                        fontSize: '6px'
+                    }
+                },
+                xaxis: {
+                    labels: {
+                        style: {
+                            fontSize: '8px'
+                        }
+                    }
+                },
+                yaxis: {
+                    labels: {
+                        style: {
+                            fontSize: '8px'
+                        }
+                    }
+                }
+            }
+        }],
+        series: [
+            {
+                name: 'Alertas Atrasados',
+                data: [6, 4, 2]
+            },
+            {
+                name: 'Alertas Totais',
+                data: [8, 8, 8]
+            }
+        ],
+        xaxis: {
+            categories: ['Data Center 1', 'Data Center 3', 'Data Center 2'],
+            labels: {
+                style: {
+                    fontSize: '15px'
+                }
+            }
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    fontSize: '15px'
+                }
+            }
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '50%',
+                endingShape: 'rounded'
+            }
+        },
+        dataLabels: {
+            enabled: true,
+            style: {
+                fontSize: '13px'
+            }
+        },
+        legend: {
+            position: 'top',
+            fontSize: '15px'
+        },
+        colors: ['#cc0000', '#00cc00']
+    };
 
-  var chart = new ApexCharts(document.querySelector("#grafico"), options);
-  chart.render();
+    var chart = new ApexCharts(document.querySelector("#grafico"), options);
+    chart.render();
 });
