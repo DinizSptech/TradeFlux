9cec2ca87b4b29958f4abebd54b5d169fc56a157
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2025-05-30 11:04:27 -0300

    Script de captura direcionada apenas a dados de capturas por enquanto

--- SlackProjeto/Python/captura/comunicacao.py
+++ SlackProjeto/Python/captura/comunicacao.py
@@ -7,12 +7,12 @@ def obter_parametros_servidor(uuid_maquina):
     return resposta.json() if resposta.status_code == 200 else {"erro": "Falha ao obter parâmetros"}
 
 def cadastrar_servidor(id_datacenter, uuidservidor, sistemaoperacional, discototal, ramtotal, processadorinfo):
-    # param_critico_cpu = 80
-    # param_critico_ram = 80
-    # param_critico_disco = 80
-    # param_atencao_cpu = 70
-    # param_atencao_ram = 70
-    # param_atencao_disco = 70
+    param_critico_cpu = 80
+    param_critico_ram = 80
+    param_critico_disco = 80
+    param_atencao_cpu = 70
+    param_atencao_ram = 70
+    param_atencao_disco = 70
     payload = {
         "id_datacenter": id_datacenter,
         "uuidservidor": uuidservidor,
@@ -20,12 +20,12 @@ def cadastrar_servidor(id_datacenter, uuidservidor, sistemaoperacional, discotot
         "discototal": discototal,
         "ramtotal": ramtotal,
         "processadorinfo": processadorinfo,
-        # "param_atencao_cpu": param_atencao_cpu,
-        # "param_atencao_ram": param_atencao_ram,
-        # "param_atencao_disco": param_atencao_disco,
-        # "param_critico_cpu": param_critico_cpu,
-        # "param_critico_ram": param_critico_ram,
-        # "param_critico_disco": param_critico_disco
+        "param_atencao_cpu": param_atencao_cpu,
+        "param_atencao_ram": param_atencao_ram,
+        "param_atencao_disco": param_atencao_disco,
+        "param_critico_cpu": param_critico_cpu,
+        "param_critico_ram": param_critico_ram,
+        "param_critico_disco": param_critico_disco
     }
     
     resposta = requests.post(f"{API_URL}/cadastrar_servidor", json=payload)
--- SlackProjeto/Python/captura/main.py
+++ SlackProjeto/Python/captura/main.py
@@ -3,51 +3,87 @@ import crawler
 import comunicacao
 import time
 
+id_datacenter = 1
+
 def main():
     print("Bem vindo ao sistema Crawler da TradeFlux\n")
     print("Essa é uma API Python que cadastra servidores e monitoriza os componentes de cada servidor.\n")
 
-    print("Escolha uma opção:\n1 - Cadastrar essa máquina no banco de dados\n2 - Coletar dados dessa máquina\n3 - Sair")
+    print("Escolha uma opção:\n1 - Iniciar coleta de dados\n2 - Sair")
     opcao = int(input("Digite a opção desejada: ")) 
-
+    
     if opcao == 1:
         info = crawler.coletar_informacoes()
+        print(info)
+        # time.sleep(10)
         uuidServidor = info['UUID da Placa Mãe']
         print("Procurando no Bando de dados uma maquina com a UUID: ", uuidServidor)
         idServidor = comunicacao.buscar_servidor(uuidServidor)
-
-        # idServidor = selectbd.coletarMaquinasDisponiveis(uuidServidor)
-        time.sleep(2)
-
         if idServidor == None:
             print("Máquina não encontrada no banco de dados.")
             print("Cadastrando máquina...")
-            SO = info["Sistema Operacional"]
+            SO_maq = info["Sistema Operacional"]
+            cpuInfo = info["Modelo do Processador"]
             ramTotal = info["RAM Máxima"]
             discoTotal = info["Armazenamento Máximo"]
-            cpuInfo = info["Modelo do Processador"]
-            insert.inserirMaquina(uuidServidor , SO, discoTotal, ramTotal, cpuInfo, idDataCenter)
+            comunicacao.cadastrar_servidor(id_datacenter, uuidServidor, SO_maq, discoTotal, ramTotal, cpuInfo)
+            # insert.inserirMaquina(uuidServidor , SO, discoTotal, ramTotal, cpuInfo, idDataCenter)
             print("Dados inseridos:")
-            print("Sistema operacional: ", SO)
+            print("Sistema operacional: ", SO_maq)
             print("RAM total: ", ramTotal)
             print("Armazenamento total: ", discoTotal)
             print("Modelo do processador: ", cpuInfo)
+            dados_parametros = comunicacao.obter_parametros_servidor(uuidServidor);
+            dados_capturados = crawler.coletar_dados(id_datacenter, dados_parametros["idservidor"])
+            while(True):
+                capturas_enviadas = comunicacao.enviar_dados(dados_capturados)
         else:
-            print("Máquina já registrada no banco")
-            print("ID da máquina: ", idServidor)
-            print("Deseja realizar a captura de dados com a nossos parametros padrões? \n1 - SIM | 2 - NÃO")
-            option = int(input())
-            if option == 1:
-                coletarDados(idServidor)
-            else:
-                print("Saindo...")
-                exit()
-
-    elif opcao == 2:   
-        dadoscapturados = crawler.coletar_dados(1,2)
-
-        print(dadoscapturados)
-    elif opcao == 3:
+            dados_capturados = crawler.coletar_dados(id_datacenter, dados_parametros["idservidor"])
+            while(True):
+                capturas_enviadas = comunicacao.enviar_dados(dados_capturados)
+            
+    elif opcao == 2:
         print("Saindo...")
         exit()
+    # print("Escolha uma opção:\n1 - Cadastrar essa máquina no banco de dados\n2 - Coletar dados dessa máquina\n3 - Sair")
+    # opcao = int(input("Digite a opção desejada: ")) 
+
+    # if opcao == 1:
+    #     info = crawler.coletar_informacoes()
+    #     uuidServidor = info['UUID da Placa Mãe']
+    #     print("Procurando no Bando de dados uma maquina com a UUID: ", uuidServidor)
+    #     idServidor = comunicacao.buscar_servidor(uuidServidor)
+
+    #     if idServidor == None:
+    #         print("Máquina não encontrada no banco de dados.")
+    #         print("Cadastrando máquina...")
+    #         SO = info["Sistema Operacional"]
+    #         ramTotal = info["RAM Máxima"]
+    #         discoTotal = info["Armazenamento Máximo"]
+    #         cpuInfo = info["Modelo do Processador"]
+    #         comunicacao.cadastrar_servidor()
+    #         # insert.inserirMaquina(uuidServidor , SO, discoTotal, ramTotal, cpuInfo, idDataCenter)
+    #         print("Dados inseridos:")
+    #         print("Sistema operacional: ", SO)
+    #         print("RAM total: ", ramTotal)
+    #         print("Armazenamento total: ", discoTotal)
+    #         print("Modelo do processador: ", cpuInfo)
+    #     else:
+    #         print("Máquina já registrada no banco")
+    #         print("ID da máquina: ", idServidor)
+    #         print("Deseja realizar a captura de dados com a nossos parametros padrões? \n1 - SIM | 2 - NÃO")
+    #         option = int(input())
+    #         if option == 1:
+    #             coletarDados(idServidor)
+    #         else:
+    #             print("Saindo...")
+    #             exit()
+
+    # elif opcao == 2:   
+    #     dadoscapturados = crawler.coletar_dados(1,2)
+
+    #     print(dadoscapturados)
+    # elif opcao == 3:
+    #     print("Saindo...")
+    #     exit()
 main()
