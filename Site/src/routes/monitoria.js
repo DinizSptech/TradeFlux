a5eaeeb5c977135084f8d9ba5c7c60002e27a0f5
DinizSptech <mateus.leite@sptech.school>
2025-06-01 12:57:39 -0300

    Mudanças da estrutura de infra. Remover pastas não utilizadas. Evolução da lógica de tempo real.

--- Site/src/routes/monitoria.js
+++ Site/src/routes/monitoria.js
@@ -1,6 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
+ let dadosTempoReal = []
+
 router.post("/monitoria", (req,res) => {
-  
+  if(dadosTempoReal.length == 0){
+    dadosTempoReal.push(req.body)
+    res.status(200).send("Novo servidor adicionado na monitoria.")
+  }
+
+  for(let i = 0; i < dadosTempoReal.length; i++){
+    let servidorAtual = dadosTempoReal[i] 
+    if(servidorAtual.servidor = req.body.servidor){
+      if (servidorAtual.length > 11){
+       servidorAtual.shift()
+       servidorAtual.push(req.body.dados[0])
+       res.status(200).send("Buffer modificado, nova captura enviada")
+      } else {
+         servidorAtual.push(req.body.dados[0])
+       res.status(200).send("Buffer modificado, nova captura enviada")
+      }
+    }
+  }
 })
--- SlackProjeto/API-Node/src/controllers/monitoria.controller.js
+++ SlackProjeto/API-Node/src/controllers/monitoria.controller.js
@@ -4,40 +4,42 @@ function enviar_captura_front(req,res){
       
            captura = {
         'servidor': req.body.nome_servidor,
-        'dados': 
-            {
-                'Momento': req.body.Momento,
-                'ram': req.body.ram_percentual,
-                'cpu': req.body.cpu_percentual,
-                'disco': req.body.disco_percentual,
-                'criticidade': 0,
-                'download': req.body.velocidade_download,
-                'upload': req.body.velocidade_upload,
-                'tempoAtivo': req.body.tempo_ativo,
-                'processos': processos
-            }
+        'dados': [
+
+          {
+            'Momento': req.body.Momento,
+            'ram': req.body.ram_percentual,
+            'cpu': req.body.cpu_percentual,
+            'disco': req.body.disco_percentual,
+            'criticidade': 0,
+            'download': req.body.velocidade_download,
+            'upload': req.body.velocidade_upload,
+            'tempoAtivo': req.body.tempo_ativo,
+            'processos': processos
+          }
+        ]
         
       }
 
        validar = ['ram', 'cpu', 'disco','download','upload']
         for(let i  = 0; i < validar.length; i++){
           if(i <= 2){
-            if(captura.dados[validar[i]] > 80){
-              captura.dados.criticidade += 3
-            } else if (captura.dados[validar[i]] > 70){
-              captura.dados.criticidade += 1
+            if(captura.dados[0][validar[i]] > 80){
+              captura.dados[0].criticidade += 3
+            } else if (captura.dados[0][validar[i]] > 70){
+              captura.dados[0].criticidade += 1
             }
           } else if (i == 3){
-           if(captura.dados[validar[i]] > 1500){
-              captura.dados.criticidade += 3
-        }else if (captura.dados[validar[i]] > 1000){
-              captura.dados.criticidade += 1
+           if(captura.dados[0][validar[i]] > 1500){
+              captura.dados[0].criticidade += 3
+        }else if (captura.dados[0][validar[i]] > 1000){
+              captura.dados[0].criticidade += 1
             }
       } else {
-                   if(captura.dados[validar[i]] > 500){
-              captura.dados.criticidade += 3
-        }else if (captura.dados[validar[i]] > 300){
-              captura.dados.criticidade += 1
+                   if(captura.dados[0][validar[i]] > 500){
+              captura.dados[0].criticidade += 3
+        }else if (captura.dados[0][validar[i]] > 300){
+              captura.dados[0].criticidade += 1
             }
       } 
 
