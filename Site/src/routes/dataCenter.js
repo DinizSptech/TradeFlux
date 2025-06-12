f4764ddd1598d9511521075e96b8d1a891092a49
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2025-06-11 23:53:45 -0300

    API Node agora envia dados a cada 60 registros e não mais 4

--- Site/src/routes/dataCenter.js
+++ Site/src/routes/dataCenter.js
@@ -58,6 +58,7 @@ router.post("/pegarServidores", async function (req, res) {
 router.post("/pegarProcessos", async function (req, res) {
   // Link da Lambda feita na AWS:
   const lambdaUrl = "https://dc6djea6zumywibfwj4z7wlale0sbvhi.lambda-url.us-east-1.on.aws/";
+  console.log('Entrou na rota')
 
   try {
     // Chama a lambda (Passa o datacenter 1 como parâmetro ou o que tiver dentro do corpo)
